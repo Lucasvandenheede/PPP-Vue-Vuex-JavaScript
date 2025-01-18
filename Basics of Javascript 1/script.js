653e22c4d9694c03c91e96f604c446d06bc6f373
Lucasvandenheede <r0950888@student.thomasmore.be>
2025-01-18 18:44:18 +0100

    Finished 9.

--- Basics of Javascript 1/script.js
+++ Basics of Javascript 1/script.js
@@ -1,4 +1,4 @@
-let fruit = "apple";
+/* let fruit = "apple";
 
 console.log("Micheal");
 console.log(23);
@@ -9,6 +9,7 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable Name Conventions
 let person = "Micheal";
 let PI = 3.1415;
 
@@ -18,4 +19,23 @@ let currentJob = "teacher";
 let job1 = "programmer";
 let job2 = "teacher";
 
-console.log(firstJob);
+console.log(firstJob); */
+
+let appleIsFruit = true;
+console.log(appleIsFruit);
+console.log(typeof appleIsFruit);
+console.log(typeof "Micheal");
+console.log(typeof 223);
+
+appleIsFruit = "absolutely";
+console.log(typeof appleIsFruit); // Dynamic Typing
+
+let age;
+console.log(age);
+console.log(typeof age);
+
+age = 26;
+console.log(age);
+console.log(typeof age);
+
+console.log(typeof null);
