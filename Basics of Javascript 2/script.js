b2dc945b23ca14693e9cd6cf82cf43a2fcdc2276
Lucasvandenheede <r0950888@student.thomasmore.be>
2025-01-31 22:01:27 +0100

    24. Finished

--- Basics of Javascript 2/script.js
+++ Basics of Javascript 2/script.js
@@ -1,5 +1,5 @@
 // 23. Strict Mode
-"use strict";
+/* "use strict";
 
 let isAvailable = true;
 let inMeeting = true;
@@ -10,4 +10,27 @@ else console.log("Person is in meeting");
 
 // const public = "Canada";
 // const private = "room 15";
-// const switch = "open";
+// const switch = "open"; */
+
+// 24. Functions
+function welcomeText() {
+  console.log("Welcome to my page!");
+}
+
+welcomeText("Hello"); // invoking, running, calling
+welcomeText();
+welcomeText();
+
+function calculator(number1, number2) {
+  console.log(number1, number2);
+  const addition = number1 + number2;
+  const resultText = `${number1} + ${number2} = ${addition}`; // 10 + 20 = 30
+
+  return resultText;
+}
+
+const result1 = calculator(12, 24);
+console.log(result1);
+
+const result2 = calculator(100, 200);
+console.log(result2);
