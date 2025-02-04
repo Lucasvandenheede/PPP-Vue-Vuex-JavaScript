301d32b505fa3db116517f54b34c0389b55da28a
Lucasvandenheede <r0950888@student.thomasmore.be>
2025-02-05 00:21:32 +0100

    63. Finished

--- Data Stuctures, Modern Operators, Strings/script.js
+++ Data Stuctures, Modern Operators, Strings/script.js
@@ -6,17 +6,39 @@ const company = {
   categories: ['Home Appliances', 'Computers', 'Televisions', 'Electronics'],
   recentProducts: ['Fridge', 'Laptop', 'Earpad', 'Monitor'],
   popularProducts: ['Keyboard', 'Mouse', 'Dishwasher'],
+  numberOfEmployees: {
+    development: {
+      active: 50,
+      onVacation: 4,
+    },
+    humanResources: {
+      active: 25,
+      onVacation: 26,
+    },
+    finance: {
+      active: 35,
+      onVacation: 30,
+    },
+  },
   getProducts: function (recentIndex, popularIndex) {
     return [
       this.recentProducts[recentIndex],
       this.popularProducts[popularIndex],
     ];
   },
+  buyProducts: function ({
+    price = '50',
+    quantity = '1',
+    recentIndex,
+    popularIndex,
+  }) {
+    console.log(price, quantity, recentIndex, popularIndex);
+  },
 };
 
 //-----// Array Destructuring //-----//
 
-// const testArray = [1, 2, 3];
+/* // const testArray = [1, 2, 3];
 // const firstElement = testArray[0];
 // const secondElement = testArray[1];
 // const thirdElement = testArray[2];
@@ -44,6 +66,40 @@ let [x, , [y, z]] = nestedArray;
 console.log(x, y, z);
 
 [x = 0, y = 0, z = 0] = [1];
-console.log(x, y, z);
+console.log(x, y, z); */
 
-//-----// Objects Destructuring //-----//
+//-----// Object Destructuring //-----//
+
+const { name, categories, numberOfEmployees } = company;
+console.log(name, categories, numberOfEmployees);
+
+const {
+  name: companyName,
+  numberOfEmployees: employees,
+  categories: saleFields,
+} = company;
+console.log(companyName, employees, saleFields);
+
+const { boardOfManagement = [], recentProducts: newProducts = [] } = company;
+console.log(boardOfManagement, newProducts);
+
+let var1 = 10;
+let var2 = 20;
+const testObject = {
+  var1: 50,
+  var2: 80,
+  var3: 100,
+};
+
+({ var1, var2 } = testObject);
+console.log(var1, var2);
+
+const {
+  development: { active: activeEmployees, onVacation: onVacationEmployees },
+} = numberOfEmployees;
+console.log(activeEmployees, onVacationEmployees);
+
+company.buyProducts({
+  recentIndex: 1,
+  popularIndex: 2,
+});
