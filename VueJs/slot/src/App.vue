15edf0e86d14fef49ab7ab3b0ecc7977a35f1d7e
Lucasvandenheede <r0950888@student.thomasmore.be>
2025-02-08 00:19:57 +0100

    Section 18

---
+++ VueJs/slot/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <Card>
+      <template #cardImg>
+        <img
+          src="https://images.pexels.com/photos/30092951/pexels-photo-30092951/free-photo-of-classic-mclaren-racing-car-on-display.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        />
+      </template>
+      <h1>I am body</h1>
+      <template #cardFooter>Hello I am footer</template>
+    </Card>
+    <Hello>
+      <template v-slot:helloHeader>I am Header</template>
+      <template #helloFooter>I am Footer</template>
+      <h1>Hello</h1>
+      <p>How are you</p>
+      <button>Click</button>
+      <Test />
+    </Hello>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue';
+import Hello from './components/Hello.vue';
+import Test from './components/Test.vue';
+
+export default {
+  name: 'App',
+  components: { Hello, Test, Card },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 100%;
+}
+</style>
---
+++ VueJs/slot/src/components/Hello.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sayHello">
+    <div class="header">
+      <slot name="helloHeader">Header</slot>
+    </div>
+    <div class="body">
+      <slot></slot>
+    </div>
+    <div class="footer">
+      <slot name="helloFooter">Footer</slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+};
+</script>
+
+<style scoped>
+.sayHello {
+  padding: 20px;
+  border: 1px solid red;
+}
+
+.header {
+  background-color: brown;
+  color: aliceblue;
+}
+
+.body {
+  background-color: blueviolet;
+}
+
+.footer {
+  background-color: blue;
+  color: aliceblue;
+}
+</style>
