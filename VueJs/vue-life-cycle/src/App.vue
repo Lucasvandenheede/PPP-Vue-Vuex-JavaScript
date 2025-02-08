e1b5607ad4227d530fee367863cde60653b932d3
Lucasvandenheede <r0950888@student.thomasmore.be>
2025-02-08 01:34:57 +0100

    Section 21

---
+++ VueJs/vue-life-cycle/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrapper">
+    <h1>LifeCycles Hook</h1>
+    <img id="myImg" src="./assets/logo.png" alt="Vue logo" />
+    <Hooks v-if="show"></Hooks>
+    <br />
+    <button @click="show = !show">Show/Hide</button>
+  </div>
+</template>
+
+<script>
+import Hooks from './components/Hooks.vue';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      show: true,
+      name: 'Lucas Vanden Heede',
+    };
+  },
+  beforeCreate() {
+    const myImg = document.getElementById('myImg');
+    console.log('myImg', myImg);
+    console.log(this.name);
+  },
+  created() {
+    const myImg = document.getElementById('myImg');
+    console.log('myImg', myImg);
+    console.log(this.name);
+  },
+  beforeMount() {
+    const myImg = document.getElementById('myImg');
+    console.log('myImg', myImg);
+    console.log(this.name);
+  },
+  mounted() {
+    const myImg = document.getElementById('myImg');
+    console.log('myImg', myImg);
+    console.log(this.name);
+  },
+  components: { Hooks },
+};
+</script>
+
+<style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ VueJs/vue-life-cycle/src/components/Hooks.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <p>{{ counter }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hooks',
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+  },
+  beforeUpdate() {
+    console.log(
+      `Counter value is about to change from ${this.counter} to ${
+        this.counter + 1
+      }`
+    );
+  },
+  updated() {
+    console.log(
+      `Counter value has changed from ${this.counter - 1} to ${this.counter}`
+    );
+  },
+  beforeUnmount() {
+    console.log('Before Unmount');
+  },
+  unmounted() {
+    console.log('Unmounted');
+  },
+};
+</script>
+
+<style></style>
