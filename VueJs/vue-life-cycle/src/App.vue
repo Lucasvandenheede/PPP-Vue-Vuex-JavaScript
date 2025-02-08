7b495d66eec3644f0bec0da01512e5879bb6784e
Lucasvandenheede <r0950888@student.thomasmore.be>
2025-02-08 01:58:18 +0100

    Section 22

--- VueJs/vue-life-cycle/src/App.vue
+++ VueJs/vue-life-cycle/src/App.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="wrapper">
     <h1>LifeCycles Hook</h1>
-    <img id="myImg" src="./assets/logo.png" alt="Vue logo" />
+    <img ref="vueImg" id="myImg" src="./assets/logo.png" alt="Vue logo" />
     <Hooks v-if="show"></Hooks>
     <br />
     <button @click="show = !show">Show/Hide</button>
+
+    <h1>Refs</h1>
+    <button @click="$refs.vueImg.style.width = '600px'">Grow</button>
+    <button @click="$refs.vueImg.style.width = '300px'">Shrink</button>
+
+    <h1>Multiple Refs</h1>
+    <p ref="myElement" v-for="item in myArr" :key="item">{{ item }}</p>
+
+    <h1>Computed</h1>
+    <div>
+      <p>{{ fullName }}</p>
+      <p>{{ reverseFullName }}</p>
+    </div>
+
+    <div>
+      <h1>{{ total }}</h1>
+      <h3>{{ totalResult }}</h3>
+      <button @click="total += 1">Click</button>
+    </div>
+
+    <h1>Watch</h1>
+    <h1 ref="counterRef">{{ counter }}</h1>
+    <button @click="counter++">Click</button>
   </div>
 </template>
 
@@ -17,6 +40,11 @@ export default {
     return {
       show: true,
       name: 'Lucas Vanden Heede',
+      myArr: [1, 2, 3],
+      firstName: 'Lucas',
+      lastName: 'Vanden Heede',
+      total: 0,
+      counter: 0,
     };
   },
   beforeCreate() {
@@ -36,8 +64,36 @@ export default {
   },
   mounted() {
     const myImg = document.getElementById('myImg');
+    // myImg.style.width = '500px';
     console.log('myImg', myImg);
     console.log(this.name);
+
+    // this.$refs.vueImg.style.width = '700px';
+
+    console.log(this.$refs.myElement);
+    this.$refs.myElement[0].style.color = 'blue';
+    this.$refs.myElement[2].style.backgroundColor = 'red';
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    reverseFullName() {
+      return this.fullName.split('').reverse().join('');
+    },
+    totalResult() {
+      return this.total > 10 ? 'Bigger than 10' : 'Lower than 10';
+    },
+  },
+  watch: {
+    counter(newValue, oldValue) {
+      if (newValue > 5) {
+        this.$refs.counterRef.style.background = 'red';
+      }
+      if (oldValue > 7) {
+        this.$refs.counterRef.style.background = 'blue';
+      }
+    },
   },
   components: { Hooks },
 };
